456b2544c6369b1fedbff23ec63ee2e28076de65
mhrsntrk <m@mhrsntrk.com>
2021-01-12 18:31:37 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -88,9 +88,6 @@ export default function Footer() {
         <Link href="/gear">
           <a className="text-sm text-gray-500 hover:text-gray-600">/gear</a>
         </Link>
-        <Link href="/about">
-          <a className="text-sm text-gray-500 hover:text-gray-600">/about</a>
-        </Link>
         <Link href="/photos">
           <a className="text-sm text-gray-500 hover:text-gray-600">/photos</a>
         </Link>
