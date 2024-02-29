5db4257e6fdb43e0709502002cc1cc00c2eade0f
mhrsntrk <m@mhrsntrk.com>
2024-02-29 16:45:37 +0100

    feat. updated crypto page

--- pages/swissknife/crypto.js
+++ pages/swissknife/crypto.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
 import {
-  getBitcoin,
   getEthereum,
   getEnergyWeb,
   getAvax,
@@ -12,28 +11,21 @@ import {
 import Container from '@/components/Container';
 import CryptoCard from '@/components/CryptoCard';
 
-export default function Crypto({
-  bitcoin,
-  ethereum,
-  energyWeb,
-  avax,
-  mina,
-  polkadot
-}) {
+export default function Crypto({ ethereum, energyWeb, avax, mina, polkadot }) {
   return (
     <Container>
       <NextSeo
         title="Crypto – mhrsntrk"
         description={`You can find the cryptocurrencies that
         I currently follow and hold below. I have used coinpaprika's API to
-        get current prices, 24 hour change and all time high data.`}
+        get current prices, 24 hour change and ATH data.`}
         canonical="https://mhrsntrk.com/swissknife/crypto"
         openGraph={{
           url: 'https://mhrsntrk.com/swissknife/crypto',
           title: 'Crypto – mhrsntrk',
           description: `You can find the cryptocurrencies that
           I currently follow and hold below. I have used coinpaprika's API to
-          get current prices, 24 hour change and all time high data.`
+          get current prices, 24 hour change and ATH data.`
         }}
       />
       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
@@ -43,57 +35,51 @@ export default function Crypto({
         <div className="mb-16">
           <p className="text-gray-600 dark:text-gray-400">
             New world, new currencies! I am deeply interested in
-            cryptocurrencies and blockchain since 2017. You can find the cryptocurrencies that
-            I currently follow and invest below. I have used coinpaprika's API to
-            get current prices, 24 hour change and all time high data.
+            cryptocurrencies and blockchain since 2017. You can find the
+            cryptocurrencies that I currently follow and invest below. I have
+            used coinpaprika's API to get current prices, 24 hour change and ATH
+            data.
           </p>
         </div>
         <div className="grid w-full grid-cols-1 gap-4 my-2 sm:grid-cols-2">
           <CryptoCard
-            name={bitcoin.name}
-            symbol={bitcoin.symbol}
-            price={bitcoin.quotes.USD.price.toFixed(2)}
-            change={bitcoin.quotes.USD.percent_change_24h}
-            ath={bitcoin.quotes.USD.ath_price.toFixed(2)}
+            name={polkadot.name}
+            symbol={polkadot.symbol}
+            price={polkadot.quotes.USD.price.toFixed(2)}
+            change={polkadot.quotes.USD.percent_change_24h}
+            ath={polkadot.quotes.USD.ath_price.toFixed(2)}
           />
           <CryptoCard
             name={ethereum.name}
             symbol={ethereum.symbol}
-            price={ethereum.quotes.USD.price.toFixed(3)}
+            price={ethereum.quotes.USD.price.toFixed(2)}
             change={ethereum.quotes.USD.percent_change_24h}
-            ath={ethereum.quotes.USD.ath_price.toFixed(3)}
+            ath={ethereum.quotes.USD.ath_price.toFixed(2)}
           />
         </div>
         <div className="grid w-full grid-cols-1 gap-4 my-2 sm:grid-cols-2">
           <CryptoCard
             name={energyWeb.name}
             symbol={energyWeb.symbol}
-            price={energyWeb.quotes.USD.price.toFixed(6)}
+            price={energyWeb.quotes.USD.price.toFixed(2)}
             change={energyWeb.quotes.USD.percent_change_24h}
-            ath={energyWeb.quotes.USD.ath_price.toFixed(6)}
+            ath={energyWeb.quotes.USD.ath_price.toFixed(2)}
           />
           <CryptoCard
-            name={polkadot.name}
-            symbol={polkadot.symbol}
-            price={polkadot.quotes.USD.price.toFixed(5)}
-            change={polkadot.quotes.USD.percent_change_24h}
-            ath={polkadot.quotes.USD.ath_price.toFixed(5)}
+            name={mina.name}
+            symbol={mina.symbol}
+            price={mina.quotes.USD.price.toFixed(2)}
+            change={mina.quotes.USD.percent_change_24h}
+            ath={mina.quotes.USD.ath_price.toFixed(2)}
           />
         </div>
         <div className="grid w-full grid-cols-1 gap-4 my-2 sm:grid-cols-2">
           <CryptoCard
-            name={mina.name}
-            symbol={mina.symbol}
-            price={mina.quotes.USD.price.toFixed(5)}
-            change={mina.quotes.USD.percent_change_24h}
-            ath={mina.quotes.USD.ath_price.toFixed(5)}
-          />
-          <CryptoCard
             name={avax.name}
             symbol={avax.symbol}
-            price={avax.quotes.USD.price.toFixed(6)}
+            price={avax.quotes.USD.price.toFixed(2)}
             change={avax.quotes.USD.percent_change_24h}
-            ath={avax.quotes.USD.ath_price.toFixed(6)}
+            ath={avax.quotes.USD.ath_price.toFixed(2)}
           />
         </div>
       </div>
@@ -102,7 +88,6 @@ export default function Crypto({
 }
 
 export async function getServerSideProps() {
-  const bitcoin = await getBitcoin();
   const ethereum = await getEthereum();
   const energyWeb = await getEnergyWeb();
   const avax = await getAvax();
@@ -110,6 +95,6 @@ export async function getServerSideProps() {
   const polkadot = await getPolkadot();
 
   return {
-    props: { bitcoin, ethereum, energyWeb, avax, mina, polkadot }
+    props: { ethereum, energyWeb, avax, mina, polkadot }
   };
 }
