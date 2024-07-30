3b2c849f425274fba70c2c3771ba532d1f027b9a
mhrsntrk <m@mhrsntrk.com>
2024-07-30 17:14:19 +0300

    fix. analytics tracker code

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/global.css';
+import Analytics from '@/lib/analytics';
 
 import { ThemeProvider } from 'next-themes';
 import { DefaultSeo } from 'next-seo';
@@ -8,7 +9,12 @@ import SEO from '../next-seo.config';
 
 function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme = 'system' enableColorScheme = {true}>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableColorScheme={true}
+    >
+      <Analytics />
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
